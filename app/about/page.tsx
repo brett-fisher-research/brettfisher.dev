96fd0da4b42a2af9fcb5d8ff0af2ed186332c55d
Brett Fisher <brett@fisherresearch.dev>
2026-07-14 19:41:21 -0700

    Update about section and add resume

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -15,10 +15,19 @@ export default function AboutPage() {
     <article className="post-content">
       <h1>About</h1>
       <p>
-      I write a lot, usually not on this blog. The perspective I try to take is that "essay" means a "try" or "attempt".
+      Hey, I'm Brett 👋 I'm a full-stack software engineer in Seattle, Washington. I first learned to code in high school
+      by writing Perl scripts in Notepad, and since then I've worked full-time at Amazon and multiple startups in the 
+      Seattle area. Now I do contract work for clients needing custom web applications.
       </p> 
       <p>
-      I'm not writing to get an A in a class. I write to write.
+      When I'm not coding, I enjoy getting outside, cooking, reading, writing, and playing way too much chess.
+      </p>
+      <h3>Resume</h3>
+      You can <a href="/BrettFisher_resume.pdf" target="_blank">download my resume here</a>.
+      <h3>Contact me</h3>
+      <p>
+      If you'd like to contact me about business opportunities, have a question, or just have something nice to say,
+      please <a href="https://forms.gle/WBsaNe5QzNPedBGv6" target="_blank">contact me here</a>.
       </p>
     </article>
   );
